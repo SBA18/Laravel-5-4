3dd6bc61eed525dc2e40b1ca645c5c90816d39b8
Sawers <sawers@localhost.com>
2017-02-22 05:54:10 +0100

    Added about me page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">About Me</div>
+
+                    <div class="panel-body">
+                        <em>Author : Sawers</em>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dicta doloribus earum enim est, eum explicabo facilis fugit iste iusto labore laborum modi quibusdam saepe sapiente similique tempora tempore.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam beatae culpa cupiditate debitis, dicta est facilis molestiae, mollitia nihil numquam pariatur provident quidem saepe sapiente, suscipit temporibus ut velit!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
