18472c349a668552d12a85d1d1e40a3a88c38e65
Sawers <sawers@localhost.com>
2017-02-23 15:30:02 +0100

    testing user profile

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,17 +3,24 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8 col-md-offset1 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">About Me</div>
 
                     <div class="panel-body">
                         <em>Author : Sawers</em>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dicta doloribus earum enim est, eum explicabo facilis fugit iste iusto labore laborum modi quibusdam saepe sapiente similique tempora tempore.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dicta doloribus earum enim est, eum explicabo facilis fugit iste iusto labore laborum modi quibusdam saepe sapiente similique tempora tempore.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dicta doloribus earum enim est, eum explicabo facilis fugit iste iusto labore laborum modi quibusdam saepe sapiente similique tempora tempore.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dicta doloribus earum enim est, eum explicabo facilis fugit iste iusto labore laborum modi quibusdam saepe sapiente similique tempora tempore.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dicta doloribus earum enim est, eum explicabo facilis fugit iste iusto labore laborum modi quibusdam saepe sapiente similique tempora tempore.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dicta doloribus earum enim est, eum explicabo facilis fugit iste iusto labore laborum modi quibusdam saepe sapiente similique tempora tempore.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis dicta doloribus earum enim est, eum explicabo facilis fugit iste iusto labore laborum modi quibusdam saepe sapiente similique tempora tempore.</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam beatae culpa cupiditate debitis, dicta est facilis molestiae, mollitia nihil numquam pariatur provident quidem saepe sapiente, suscipit temporibus ut velit!</p>
                     </div>
                 </div>
             </div>
+            @include('partials.sidebar')
         </div>
     </div>
 @endsection
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,8 @@
+<div class="col-md-4 ">
+    <div class="panel panel-default">
+        <div class="panel-heading">Sidebare</div>
+        <div class="panel-body">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur autem blanditiis corporis deleniti dolore doloremque error expedita facilis illo, incidunt odit omnis porro quasi, quisquam saepe sint? Aperiam, reprehenderit!</p>
+        </div>
+    </div>
+</div>
